4bb9ac9ab43844503693c11d69dd947450161bd7
Chris Marchetti <cmarche2ti@gmail.com>
2020-05-26 05:55:00 -0600

    added exception handling for user input

--- logisticmodel.py
+++ logisticmodel.py
@@ -7,66 +7,87 @@ import texttable as tt
 import os
 
 # Clear console on start of the program
-os.system('cls' if os.name == 'nt' else 'clear')
+os.system("cls" if os.name == "nt" else "clear")
 
 # User directions
-print('-'*24)
-print('Modeling Logistic Growth')
-print('-'*24)
-print('This program will model the spread of a rumor')
-print('Each program will take a number of people and a spread rate')
-print('(The number of people told the rumor each day')
+print("-" * 24)
+print("Modeling Logistic Growth")
+print("-" * 24)
+print("This program will model the spread of a rumor")
+print("The program will take a number of people and a spread rate")
+print("(The number of people told the rumor each day) as input.")
 print()
 
 rumor_list = []
-results_list=[(0,1)]
+results_list = [(0, 1)]
+
 
 def pause():
     print()
     input("Press Enter to continue...")
-    print() 
+    print()
+
 
 def trial(num_trials):
-    for i in range(num_trials):
+    for _ in range(num_trials):
         heard_rumor = random.randint(1, num_people)
-        print(f'Person number {heard_rumor} heard the rumor.')
-        time.sleep(.2)
+        print(f"Person number {heard_rumor} heard the rumor.")
+        time.sleep(0.2)
         if heard_rumor not in rumor_list:
             rumor_list.append(heard_rumor)
     pause()
 
-num_people = int(input('How many people are in your group? '))
-num_spread = int(input('How fast does the rumor spread? '))
+
+while True:
+    try:
+        num_people = int(input("How many people are in your group? "))
+        break
+    except:
+        print("The number of people must be a positive integer")
+        print()
+
+while True:
+    try:
+        num_spread = int(input("How fast does the rumor spread? "))
+        break
+    except:
+        print("The rate of spread must be a positive integer")
+        print()
+
+
+print()
+print(f"People in group: {num_people}")
+print(f"Rate of spread: {num_spread}")
 print()
 num_trials = 1
 round_num = 1
 
-#Round 1
-print('The first person to hear the rumor is ....')
+# Round 1
+print("The first person to hear the rumor is ....")
 trial(num_trials)
 print()
 
 # All rounds after round 1
 while len(rumor_list) != num_people:
     num_trials = len(rumor_list) * num_spread
-    print(f'In Round #{round_num}, {num_trials} people will hear the rumor.')
+    print(f"In Round #{round_num}, {num_trials} people will hear the rumor.")
     pause()
-    print(f'Round Number {round_num}')
-    print('-'*15)      
+    print(f"Round Number {round_num}")
+    print("-" * 15)
     trial(num_trials)
     print()
-    print(f'Results of Round #{round_num}')
-    print('-'*15)
-    print(f'{len(rumor_list)} people have heard the rumor:')
+    print(f"Results of Round #{round_num}")
+    print("-" * 15)
+    print(f"{len(rumor_list)} people have heard the rumor:")
     rumor_list.sort()
-    print(rumor_list)       
-    results_list.append((round_num,len(rumor_list)))
-    round_num+=1
+    print(rumor_list)
+    results_list.append((round_num, len(rumor_list)))
+    round_num += 1
     pause()
 
 # Display results table in the console
 print()
-print('Final Results Table')
+print("Final Results Table")
 print()
 
 xlist = []
@@ -76,24 +97,24 @@ for x, y in results_list:
     ylist.append(y)
 
 results_table = tt.Texttable()
-header = ['Round', 'Total']
+header = ["Round", "Total"]
 results_table.header(header)
 for x, y in results_list:
     results_table.add_row([x, y])
 
-results_table.set_cols_width([8,8])
-results_table.set_cols_align(['c','c'])
+results_table.set_cols_width([8, 8])
+results_table.set_cols_align(["c", "c"])
 s = results_table.draw()
 print(s)
 
 
-# Create graph of results 
-fig = plt.figure(figsize=[6,4])
+# Create graph of results
+fig = plt.figure(figsize=[6, 4])
 
-axes = fig.add_axes([.1,.1,.8,.8])
-axes.set_xlabel('Number of Rounds (Days)')
-axes.set_ylabel('Heard Rumor')
-axes.set_title('Spread of a Rumor')
+axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+axes.set_xlabel("Number of Rounds (Days)")
+axes.set_ylabel("Heard Rumor")
+axes.set_title("Spread of a Rumor")
 
-axes.plot(xlist, ylist, marker ="o", lw=2)
+axes.plot(xlist, ylist, marker="o", lw=2)
 plt.show()
